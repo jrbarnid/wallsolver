0a3171c137a61fe35b6d25e9ff4833002a523ebc
danburkhol <danburkhol@gmail.com>
2016-07-10 16:38:37 -0400

    fixed wall collisions, maze generation

--- wallsolverCPU.c
+++ wallsolverCPU.c
@@ -1,5 +1,6 @@
 /*	CPU Based Wallsolver 
 
+	gcc -Wall wallsolverCPU.c -o testCPU
 
 */
 
@@ -32,6 +33,9 @@ typedef struct space {
 } space;
 
 
+
+
+
 void outputBoard(space *in) {
 
 	for (int i = 0; i < SPACE_WIDTH; i++) {
@@ -53,7 +57,8 @@ int main(int argc, char const *argv[])
 {
 	//
 
-	space blankSpace = {false, false, false, false, false, false};
+	//space blankSpace = {false, false, false, false, false, false};
+	space blankSpace = {true, true, true, true, false, false};
 
 	int numSpaces = SPACE_LENGTH * SPACE_WIDTH;
 	int spaceSize = sizeof(space) * numSpaces;
@@ -78,10 +83,95 @@ int main(int argc, char const *argv[])
 		for (int j = 0; j < WALL_LENGTH; j++) {
 			int idx = (i * WALL_LENGTH) + j; 	// == walls[i][j];
 
-			//printf("wall idx: %d\n", idx);
-
-			walls[i + j] = (wall)(rand() % 4);
+			walls[idx] = (wall)(rand() % 4);
 			
+			printf("IDX %d - %d\n", idx, walls[idx]);
+		}
+
+	}
+
+	/* 	Make sure no walls overlap
+		For each wall, identify the neighboring walls if they exist
+		determine if a neighbor caused a conflict and while conflicts
+		exist, randomize a new wall
+	*/
+	for (int i = 0; i < WALL_WIDTH; i++) {
+
+		for (int j = 0; j < WALL_LENGTH; j++) {
+			int idx = (i * WALL_LENGTH) + j;
+
+			bool colUp = false;
+			bool colDown = false;
+			bool colLeft = false;
+			bool colRight = false;
+
+			wall *up = 0;
+			wall *down = 0;
+			wall *left = 0;
+			wall *right = 0;
+
+
+			if (j < 4) {
+				right = walls[idx + 1];
+				colRight = (walls[idx] == RIGHT) && (right == LEFT);
+			}
+
+			if (j > 0) {
+				left = walls[idx - 1];
+				colLeft = (walls[idx] == LEFT) && (left == RIGHT);
+			}
+
+			if (i < 4) {
+				down = walls[idx + WALL_LENGTH];
+				colDown = (walls[idx] == DOWN) && (down == UP);
+			} 
+
+			if (i > 0) {
+				up = walls[idx - WALL_LENGTH];
+				colUp = (walls[idx] == UP) && (up == DOWN);
+			}
+
+
+			while (colUp || colDown || colLeft || colRight) {
+				printf("IDX No Overlap: %d - %d\n", idx, walls[idx]);
+				walls[idx] = (wall)(rand() % 4);
+
+				if (j < 4) {
+					colRight = (walls[idx] == RIGHT) && (right == LEFT);
+				}
+
+				if (j > 0) {
+					colLeft = (walls[idx] == LEFT) && (left == RIGHT);
+				}
+
+				if (i < 4) {
+					colDown = (walls[idx] == DOWN) && (down == UP);
+				} 
+
+				if (i > 0) {
+					colUp = (walls[idx] == UP) && (up == DOWN);
+				}
+
+
+			}
+
+		}
+
+	}
+
+
+	/* 	Generate the board
+		For each wall, identify the board spaces that it effects
+		Determine the effect of each affected space's mobility
+	*/
+
+
+
+	for (int i = 0; i < WALL_WIDTH; i++) {
+		for (int j = 0; j < WALL_LENGTH; j++) {
+			int idx = (i * WALL_LENGTH) + j;
+
+			printf("Maze Generated: %d - %d\n", idx, walls[idx]);
 
 			// Space idx that this wall affects
 			// Per wall, figure out the adjacent spaces this wall effects
@@ -92,24 +182,25 @@ int main(int argc, char const *argv[])
 			BL = TL + SPACE_LENGTH;
 			BR = BL + 1;
 
+			//printf("TL: %d, TR: %d, BL: %d, BR: %d\n", TL, TR, BL, BR);
 
-			board[TL].right = walls[idx] == UP;
-			board[TL].down = walls[idx] == LEFT;
+			/*
+			board[TL].right = (board[TL].right) && (walls[idx] != UP);
+			board[TL].down = (board[TL].down) && (walls[idx] != LEFT);
 
-			board[TR].left = walls[idx] == UP;
-			board[TR].down = walls[idx] == RIGHT;
+			board[TR].left = (walls[idx] != UP);
+			board[TR].down = (walls[idx] != RIGHT);
 
-			board[BL].right = walls[idx] == DOWN;
-			board[BL].up = walls[idx] == LEFT;
+			board[BL].right = (walls[idx] != DOWN);
+			board[BL].up = (walls[idx] != LEFT);
 
-			board[BR].left = walls[idx] == UP;
-			board[BR].up = walls[idx] == RIGHT;
-
-
+			board[BR].left = (walls[idx] != UP);
+			board[BR].up = (walls[idx] != RIGHT);
+			*/
 		}
-
 	}
 
+	// Generate Board
 	board[0].start = true;
 	board[numSpaces - 1].finish = true;
 
